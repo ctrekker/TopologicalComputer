26f170ebbf2c72576c89d80ba437ddf23681540a
Connor Burns <ctrekker4@gmail.com>
2019-02-11 17:51:56 -0700

    Remove legacy print_progress_bar function

--- gradient_2d.py
+++ gradient_2d.py
@@ -3,28 +3,7 @@ import random
 from vector import *
 import math
 import numpy as np
-import sys
-
-def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='='):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + ' ' * (length - filledLength)
-    sys.stdout.write('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix))
-    # Print New Line on Complete
-    if iteration == total:
-        print()
-    sys.stdout.flush()
+import util
 
 size = 300
 gradient = np.fromfunction(np.vectorize(lambda x: -abs(x-size/2)+size/2+(1/100.0)*((x-size/2))**2), (size,), dtype=np.float32)
@@ -93,7 +72,7 @@ for terrain_point_index in range(len(gradient)):
 terrain_csv_line += '\n'
 
 for frame_number in range(frame_count):
-    print_progress_bar(frame_number, frame_count, prefix=str(frame_number)+'/'+str(frame_count))
+    util.print_progress_bar(frame_number, frame_count, prefix=str(frame_number)+'/'+str(frame_count))
     for signal_index in range(len(signals)):
         s = signals[signal_index]
         if math.isnan(s.position.y):
